fc20d8858d3d1e832240bf7e72a32dd084df986b
Matthieu BRAULT <matthieu.brault@epitech.eu>
2020-04-21 23:13:57 +0200

    deleting useless files

--- MP/app/src/main/java/ca/ulaval/ima/mp/MainActivity.java
+++ MP/app/src/main/java/ca/ulaval/ima/mp/MainActivity.java
@@ -19,13 +19,7 @@ public class MainActivity extends AppCompatActivity implements ResaurantListFrag
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        /*AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_map, R.id.navigation_list, R.id.navigation_profile)
-                .build();*/
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
