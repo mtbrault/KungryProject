7d6b81f0f63311fe69e5fc6ea28109f89d724da0
Mehdi Makhloufi <mehdi.makhloufi@epitech.eu>
2020-04-22 01:12:54 +0200

    Push header

--- MP/app/src/main/java/ca/ulaval/ima/mp/MainActivity.java
+++ MP/app/src/main/java/ca/ulaval/ima/mp/MainActivity.java
@@ -2,9 +2,11 @@ package ca.ulaval.ima.mp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -18,6 +20,13 @@ public class MainActivity extends AppCompatActivity implements ResaurantListFrag
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        getSupportActionBar().setElevation(0);
+        View view = getSupportActionBar().getCustomView();
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
