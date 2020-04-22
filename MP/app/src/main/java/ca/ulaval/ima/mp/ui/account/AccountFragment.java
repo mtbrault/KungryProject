a47ed079bb3aa88a12ed2d9a9e6215057771977d
Mehdi Makhloufi <mehdi.makhloufi@epitech.eu>
2020-04-22 16:46:11 +0200

    First try for login

--- MP/app/src/main/java/ca/ulaval/ima/mp/ui/account/AccountFragment.java
+++ MP/app/src/main/java/ca/ulaval/ima/mp/ui/account/AccountFragment.java
@@ -1,5 +1,8 @@
 package ca.ulaval.ima.mp.ui.account;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,7 +10,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
@@ -15,9 +17,23 @@ import ca.ulaval.ima.mp.R;
 
 public class AccountFragment extends Fragment {
 
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        sharedPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        super.onAttach(context);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        String token = sharedPreferences.getString("token", null);
         View root = inflater.inflate(R.layout.fragment_account, container, false);
+
+        if (token == null) {
+            root = inflater.inflate(R.layout.fragment_login, container, false);
+        }
         final TextView textName = root.findViewById(R.id.name);
         final TextView textEmail = root.findViewById(R.id.email);
         final TextView textNumber = root.findViewById(R.id.number);
